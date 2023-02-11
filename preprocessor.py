906981ca47b9ad81d83ec74761af325c0439f313
Kevin <zhijunwang2002@gmail.com>
2023-02-11 16:44:38 -0500

    save code from main project

--- preprocessor.py
+++ preprocessor.py
@@ -521,3 +521,97 @@ class PreProcessor:
         clauses.append(self._negated_conclusion[0])
 
         return clauses
+
+
+# DEBUG = False
+#
+#
+# def normalize(argument: List[Formula], is_proof: bool) -> List[str]:
+#     normalizer = Normalizer(argument)
+#     arg = normalizer.get_arg()
+#
+#     if is_proof:
+#         if DEBUG:
+#             print("Sub step 1. negating conclusion")
+#         normalizer.negate_conclusion()
+#         if DEBUG:
+#             normalizer.print_argument()
+#             print("")
+#
+#     if DEBUG:
+#         print("Sub step 2. removing arrows")
+#     for f, formula in enumerate(arg):
+#         arg[f] = normalizer.remove_arrows(formula)
+#     if DEBUG:
+#         normalizer.print_argument()
+#         print("")
+#
+#     if DEBUG:
+#         print("Sub step 3. moving negation inward")
+#     for f, formula in enumerate(arg):
+#         formula_type = formula.get_formula_type()
+#         var_count = formula.get_var_count()
+#
+#         # need to refactor the var_count set logic
+#         if formula_type == Type.UNARY:
+#             arg[f] = normalizer.move_negation_inward(formula, False)
+#             arg[f].set_quantifier(formula.get_quantifier())
+#             arg[f].set_var_count(var_count)
+#
+#         if formula_type == Type.BINARY:
+#             arg[f] = normalizer.move_negation_inward(formula, False)
+#             arg[f].set_var_count(var_count)
+#     if DEBUG:
+#         normalizer.print_argument()
+#         print("")
+#
+#     if DEBUG:
+#         print("Sub step 4. standardizing variables")
+#     for f, formula in enumerate(arg):
+#         var_count = formula.get_var_count()
+#
+#         for var in var_count:
+#             normalizer.set_subscript(0)
+#             arg[f] = normalizer.standardize_variables(formula, var)
+#     if DEBUG:
+#         normalizer.print_argument()
+#         print("")
+#
+#     if DEBUG:
+#         print("Sub step 5. moving all quantifiers to front")
+#     for f, formula in enumerate(arg):
+#         arg[f].set_quant_list(
+#             normalizer.move_quantifiers_to_front(formula, [])
+#         )
+#     if DEBUG:
+#         normalizer.print_argument()
+#         print("")
+#
+#     if DEBUG:
+#         print("Sub step 6. skolemizing formulas")
+#     for f, formula in enumerate(arg):
+#         drop_list = []
+#
+#         # dropping the existentials in the quantifier list
+#         quant_list = formula.get_quant_list()
+#         for q, quant_holder in enumerate(quant_list.copy()):
+#             quantifier = quant_holder[0]
+#             quant_var = quant_holder[1]
+#             if len(quant_list) > 1:
+#                 prev_var = quant_list[q - 1][1]
+#             else:
+#                 prev_var = ""
+#             if (quantifier == Quantifier.EXISTENTIAL
+#                     and quant_var not in quant_list):
+#                 drop_list.append((quant_var, prev_var))
+#                 quant_list.pop(q)
+#                 arg[f].set_quant_list(quant_list)
+#
+#         # skolemize each variable in the formula
+#         for to_drop in drop_list:
+#             arg[f] = normalizer.skolemize(formula, to_drop)
+#     if DEBUG:
+#         normalizer.print_argument()
+#         print("")
+#
+#     return normalizer.to_string()
